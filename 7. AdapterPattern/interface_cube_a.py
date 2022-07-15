a11d9b6a331764c7a14480691f594ad59ef0197e
markb <markbanford@gmail.com>
2022-07-15 14:11:59 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	1. FactoryPattern/chair_factory.py

---
+++ 7. AdapterPattern/interface_cube_a.py
@@ -0,0 +1,10 @@
+from abc import ABCMeta, abstractmethod
+
+
+class ICubeA(metaclass=ABCMeta):
+    "An interface for an object"
+
+    @staticmethod
+    @abstractmethod
+    def manufacture(width, height, depth):
+        "manufactures a cube"
