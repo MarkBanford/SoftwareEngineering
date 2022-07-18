1705f23874727ecab1fb85407d95a07fa6e53ce3
markb <markbanford@gmail.com>
2022-07-18 12:05:07 +0100

    1

---
+++ 10. FlyweightPattern/flyweight.py
@@ -0,0 +1,5 @@
+class Flyweight():  # pylint: disable=too-few-public-methods
+    "The Flyweight that contains an intrinsic value called code"
+
+    def __init__(self, code: int) -> None:
+        self.code = code
